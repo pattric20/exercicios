ef47bbb232dde82e6ae7030f3bcc7d4a4753235a
Pattric Vintter <pattric.faria.pb@compasso.com.br>
2024-12-26 11:00:26 -0300

    Copy main for exe_bank

--- exercicios_1semana/src/conceitosPOO/exe_bank.java
+++ exercicios_1semana/src/conceitosPOO/exe_bank.java
@@ -1,5 +1,7 @@
 package conceitosPOO;
 
+import java.util.Scanner;
+
 public class exe_bank {
     private int conta;
     private String name;
@@ -37,5 +39,39 @@ public class exe_bank {
     public void saque(float saque){
         float total = (float) (getSaldo() - saque - 5.00);
         setSaldo(total);
+    }
+
+
+    /* Esta função foi feita para diminuir a quantidade de codigo no main,
+     pois ele será usado em outros programas*/
+    public void init_app(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o nome do titular dan conta: ");
+        String name = sc.nextLine();
+        System.out.print("Digite o numero da conta: ");
+        int contaID = sc.nextInt();
+        exe_bank conta;
+        System.out.print("Quer fazer um deposito inicial? (y ou n): ");
+        char dep = sc.next().charAt(0);
+        if(dep == 'y') {
+            System.out.print("Digite o valor: ");
+            float depositoIni = sc.nextFloat();
+            conta = new exe_bank(contaID,name,depositoIni);
+        }else{
+            conta = new exe_bank(contaID,name);
+        }
+
+        System.out.println(conta.toString());
+
+        System.out.print("\n\nDigite o valor para deposito: ");
+        conta.deposito(sc.nextFloat());
+
+        System.out.println(conta.toString());
+
+        System.out.print("\n\nDigite o valor para saque: ");
+        conta.saque(sc.nextFloat());
+
+        System.out.println(conta.toString());
+        sc.close();
     }
 }
