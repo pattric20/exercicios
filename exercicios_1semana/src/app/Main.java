656eb73f022888a259e9c3a345baa74aa30620cd
Pattric Vintter <pattric.faria.pb@compasso.com.br>
2024-12-27 21:24:23 -0300

    Exercicio proposto lista

--- exercicios_1semana/src/app/Main.java
+++ exercicios_1semana/src/app/Main.java
@@ -10,31 +10,8 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        ListEmployees lista = new ListEmployees();
-        System.out.print("How many employees will be registered? ");
-        int N = sc.nextInt(),id;
-        for(int i=1;i<=N;i++){
-            System.out.println();
-            String name;
-            float salary;
-            System.out.println("Emplyoee #"+i+":");
-            System.out.print("ID: ");
-            id = sc.nextInt();
-            System.out.print("Name: ");
-            sc.nextLine();
-            name = sc.nextLine();
-            System.out.print("Salary: ");
-            salary = sc.nextFloat();
-            Employees emp = new Employees(id,name,salary);
-            lista.addEmplyoee(emp);
-        }
-
-        System.out.println("\nEnter the employee id that will have salary increase: ");
-        id = sc.nextInt();
-        System.out.println("Enter the percentage: ");
-        float percent = sc.nextFloat();
-        lista.increaseSalary(id,percent);
-        System.out.println();
-        lista.printListEmployees();
+        int m= sc.nextInt(),n= sc.nextInt();
+        Matriz mat = new Matriz(m,n);
+        mat.printPosViz(sc.nextInt());
     }
 }
