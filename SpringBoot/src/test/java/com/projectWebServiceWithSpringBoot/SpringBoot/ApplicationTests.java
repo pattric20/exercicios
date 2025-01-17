abb96bf93c2617e42cf81e1326beb7f0802edc49
Pattric Vintter <pattric.faria.pb@compasso.com.br>
2025-01-17 16:05:43 -0300

    Created project SpringBoot

---
+++ SpringBoot/src/test/java/com/projectWebServiceWithSpringBoot/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectWebServiceWithSpringBoot.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
